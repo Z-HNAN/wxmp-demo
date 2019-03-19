55d45d3c8ada1ce4e033bcc307772d1d59337440
LWW <709408675@qq.com>
2019-03-19 11:57:15 +0800

    first commit

--- pages/post/show.js
+++ pages/post/show.js
@@ -1,18 +1,82 @@
-// pages/post/show2.js
+// pages/post/show.js
+const app = getApp()
+let main = [
+  {
+    src: '../../icon/school.png',
+    name: '校园'
+  },
+  {
+    src: '../../icon/class.png',
+    name: '课程'
+  },
+  {
+    src: '../../icon/team.png',
+    name: '班级'
+  },
+  {
+    src: '../../icon/comment.png',
+    name: '公众号'
+  }
+]
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    right_icon: "../../icon/right_icon.png",
+    icon_style: 'float:left',
+    main: main,
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
 
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
   },
 
   /**
