13b5694bc34a61dbe627a0ea43d3683f17a32389
LWW <709408675@qq.com>
2019-04-09 10:01:59 +0800

    布局

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,6 +7,7 @@ Page({
   data: {
     isFold: false,
     nowToggle: '',
+    list_hide:false,
     systemList: [{
       id: 0,
       subject:'科技论文写作',
@@ -26,16 +27,49 @@ Page({
       start_time: '2019-03-15 15:32',
       deadline: '',
       image: '../../icon/down.png'
-    }
-    ]
+    }],
+    systemList_right: [{
+      idright: 0,
+      subject: '科技论文写作',
+      hidden: true,
+      title: '平时作业2-论文检索分享（小组作业）',
+      contentH: '检索小组命题相关的近三年文献并作汇报分享根据小组确定的论文题目检索相关文献201920182017打包发送至平台下周课进行论文分享PPT(ppt发送至平台提交) ，选择4-5篇介绍论文内容。ppt内容：来自哪所大学的哪些研究者，针对什么问题作了什么，解决了什么问题，取得什么效果，有什么创新点，不足处是什么，未来要做什么。',
+      start_time: '2019-03-15 15:32',
+      deadline: '',
+      image: '../../icon/down.png'
+    },
+    {
+      idright: 1,
+      subject: '大学生与法',
+      hidden: true,
+      title: '期中个人作业',
+      contentH: '搜集生活中的法律案例并加以分析，制作成PPT或word。',
+      start_time: '2019-03-15 15:32',
+      deadline: '',
+      image: '../../icon/down.png'
+    },
+    {
+      idright: 2,
+      subject: '大学生与法',
+      hidden: true,
+      title: '期中个人作业',
+      contentH: '搜集生活中的法律案例并加以分析，制作成PPT或word。',
+      start_time: '2019-03-15 15:32',
+      deadline: '',
+      image: '../../icon/down.png'
+    }]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
-
+    
+  },
+  switch_Layout:function(e){
+    this.setData({
+      list_hide: !this.data.list_hide
+    })
   },
 
   //展开更多
@@ -61,12 +95,13 @@ Page({
       console.log(systemList)
     }
   },
+  
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
