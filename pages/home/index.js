2b7fa735c78f4d28d33bc4748bcedfe5b9b6c646
Z-HNAN <zhn68033@163.com>
2019-04-09 10:46:41 +0800

    修改了home/index样式的问题

--- pages/home/index.js
+++ pages/home/index.js
@@ -8,7 +8,8 @@ Page({
     isFold: false,
     nowToggle: '',
     list_hide:false,
-    systemList: [{
+    systemList: [
+      {
       id: 0,
       subject:'科技论文写作',
       hidden: true,
@@ -27,37 +28,8 @@ Page({
       start_time: '2019-03-15 15:32',
       deadline: '',
       image: '../../icon/down.png'
-    }],
-    systemList_right: [{
-      idright: 0,
-      subject: '科技论文写作',
-      hidden: true,
-      title: '平时作业2-论文检索分享（小组作业）',
-      contentH: '检索小组命题相关的近三年文献并作汇报分享根据小组确定的论文题目检索相关文献201920182017打包发送至平台下周课进行论文分享PPT(ppt发送至平台提交) ，选择4-5篇介绍论文内容。ppt内容：来自哪所大学的哪些研究者，针对什么问题作了什么，解决了什么问题，取得什么效果，有什么创新点，不足处是什么，未来要做什么。',
-      start_time: '2019-03-15 15:32',
-      deadline: '',
-      image: '../../icon/down.png'
     },
-    {
-      idright: 1,
-      subject: '大学生与法',
-      hidden: true,
-      title: '期中个人作业',
-      contentH: '搜集生活中的法律案例并加以分析，制作成PPT或word。',
-      start_time: '2019-03-15 15:32',
-      deadline: '',
-      image: '../../icon/down.png'
-    },
-    {
-      idright: 2,
-      subject: '大学生与法',
-      hidden: true,
-      title: '期中个人作业',
-      contentH: '搜集生活中的法律案例并加以分析，制作成PPT或word。',
-      start_time: '2019-03-15 15:32',
-      deadline: '',
-      image: '../../icon/down.png'
-    }]
+    ]
   },
 
   /**
